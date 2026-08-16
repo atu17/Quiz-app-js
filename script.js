3abad64ef1efa7571d18f7d92a45f40faa4e26bd
atu17 <atu.ss.284.8823@gmail.com>
2026-08-16 15:05:28 +0900

    大枠完成

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let choiceArea = document.querySelector('.question-choices');
 
 let currentQuestionIndex = 0;
 
+let score = 0;
+
 let questionText = document.querySelector('.question');
 
 let answerMessage  = document.querySelector('.answer-message');
@@ -16,35 +18,35 @@ quizzes = [
   {
       question: "星宮ととのアルバム「TIMESURF」から、「summer dreamin'」と「夢日記」をドッキングさせた曲とは何か。",
       choices: ["なおざり","繰り返す夏休み","タイムサーフ","ともだち"],
-      answer: 3,
+      answer: "ともだち",
       explanation: "正解は、ともだち"
   },
 
   {
       question: "星宮ととの楽曲「Outline」に参加したアーティストは、誰か",
       choices: ["TEMPLIME","N33T","EAERAN&Phritz","somunia"],
-      answer: 2,
+      answer: "EAERAN&Phritz",
       explanation: "正解は、EAERAN&Phritz"
   },
     
   {
       question: "星宮ととのアルバム「SHOOTING POP」の3曲目は、何か",
       choices: ["滅亡☆タイムスリップ","打倒、侵略者！","たぶんハッピーエンド","SHOOTING POP"],
-      answer: 3,
+      answer: "SHOOTING POP",
       explanation: "正解は、SHOOTING POP"
   },
 
  {
     question: "星宮ととのアルバム「POP-AID」に収録されていない曲は何か",
     choices: ["リンクロット","Hiko","skycave","Escapism"],
-    answer: 0,
+    answer: "リンクロット",
     explanation: "正解は、リンクロット"
  },
 
 {
     question: "2026/6/7に行われた星宮ととの自主企画のライブ名は、何か",
     choices: ["MOONRAKER","TIME-SARFIN","Cloud Diver","NIGHT HIKE"],
-    answer: 0,
+    answer: "MOONRAKER",
     explanation: "正解は、MOONRAKER"
  }
 ];
@@ -62,19 +64,32 @@ function shuffle(array){
 function showQuestion()  {
   choiceArea.innerHTML = "";
   answerMessage.textContent = "";
+  nextButton.disabled = true;
+
+  if ( currentQuestionIndex === quizzes.length - 1 ) {
+        nextButton.textContent = "結果を見る";
+      } else {
+        nextButton.textContent = "次の問題へ";
+      }
+
   number.textContent = currentQuestionIndex + 1 ;
   const quiz = quizzes[currentQuestionIndex];
   questionText.textContent = quiz.question;
   for ( let i = 0; i < quiz.choices.length ;i++ ) {
     const button = document.createElement("button");
     button.textContent = quiz.choices[i];
-
     button.addEventListener("click",() => {
-       if ( quiz.answer === i ) {
-    answerMessage.textContent = "正解！ " + quiz.explanation;
+      nextButton.disabled = false;
+       const buttons = choiceArea.querySelectorAll("button");
+       for ( let j = 0; j < buttons.length; j++){
+        buttons[j].disabled = true;
+       }
+       if ( quiz.answer === quiz.choices[i] ) {
+          answerMessage.textContent = "正解！ " + quiz.explanation;
+          score++;
   } else {
     answerMessage.textContent = "不正解！ " + quiz.explanation;
-  }
+  } 
 });
     choiceArea.appendChild(button);
   }
@@ -85,7 +100,29 @@ nextButton.addEventListener("click",() => {
   if (currentQuestionIndex < quizzes.length - 1 ){
     currentQuestionIndex++;
     showQuestion();
-  }
-  
+  } else  {
+    questionText.textContent = "";
+    choiceArea.innerHTML = "";
+    number.textContent = "";
+    answerMessage.textContent = "クイズ終了 あなたの正解数は" + score + "/" + quizzes.length + "問です";
+
+    nextButton.style.display = "none";
+
+    const restartButton = document.createElement("button");
+    
+     restartButton.textContent = "もう一度挑戦";
+
+    restartButton.addEventListener("click",() => {
+      currentQuestionIndex = 0;
+      score = 0;
+      
+      quizzes = shuffle(quizzes);
+      nextButton.style.display = "inline-block";
+
+      showQuestion();
+    });
+    choiceArea.appendChild(restartButton);
+  } 
 });
+quizzes = shuffle(quizzes);
 showQuestion();
